5e5554a90ce8395dab3ec4f30316abb67fda37bc
antony-jr <antonyjr@protonmail.com>
2019-12-18 01:13:36 +0530

    basic skeleton

---
+++ components/PageLoader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {Grid} from '@material-ui/core';
+
+export default function PageLoader(){
+	return (
+		<Grid container direction="column" justify="center"
+			alignItems="center" style={{minWidth: '90vw', minHeight: '90vh'}}>
+			<CircularProgress style={{color: "black"}}/>
+		</Grid>
+	);
+}
